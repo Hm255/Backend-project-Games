fe26a63b128f71fd462e47337a5d7527f4e47de0
Hamza Moosa <hamzamoosa@Hamzas-MacBook-Air.local>
2022-10-13 14:48:56 +0100

    5-get-users-running

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -70,23 +70,6 @@ afterAll(() => {
 
 
   describe("GET/api/users", () => {
-    it('should return 1 users details as shown', () => {
-      return request (app)
-      .get('/api/users')
-      .expect(200)
-      .then(({body}) => {
-        expect(body.users[0]).toEqual(
-          {
-            username: 'mallionaire',
-            name: 'haz',
-            avatar_url:
-              'https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg'
-          }
-        );
-      });
-    
-    })
-
     it('should return all users details as shown', () => {
       return request (app)
       .get('/api/users')
@@ -121,28 +104,4 @@ afterAll(() => {
         ]);
       });
     })
-    // it('should return a 400 if the return is of an invalid type', () => {
-    //   return request (app)
-    //   .get('/api/users/46578')
-    //   .expect(400)
-    //   .then(({body}) => {
-    //     console.log(body)
-    //     expect(body.msg).toBe('ID is not valid (type is wrong)')
-    //   })
-    //})
-    it('should return a 404 if the return doesnt exist', () => {
-      return request(app)
-      .get('/api/users/999768767896876999')
-      .expect(404)
-      .then(({body}) => {
-        expect(body.msg).toBe('ID does not exist')
-      })
-    })
-  });
-
-
-
-/*
-beforeEach: this returns the test data from the linked table
-*/
-
+  })
